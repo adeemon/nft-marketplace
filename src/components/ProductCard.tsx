cc70f00fd1f9256b1334aae4887b7f9f4e81966c
Ilia Ustimov <ilia_ustimov@epam.com>
2022-12-13 21:11:07 +0400

    Почти доделана карточка товара. Проблема с бордером

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,48 @@
+import { Product } from "./testData";
+import Style from '../styles/ProductCard.module.scss'
+import Image from "next/image";
+import ethIcon from '../images/ethereum (1) 1.svg'
+
+type ImageInfo = {
+    src: string
+}
+
+const myLoader = ({src} : ImageInfo) => {
+    return src;
+}
+
+export default function ProductCard ({name, avatar, price, nftName, id, image} : Product) {
+    return (
+        <div className={Style.container}>
+            <div className={Style.mainMenu}>
+                <div className={Style.priceInfo}>
+                    <p className={Style.bid}>Price</p>
+                    <p className={Style.price}>
+                        <Image src={ethIcon} alt='Eth logo' width={16} height={16} />
+                        {price} ETH
+                        </p>
+                </div>
+                <button className={Style.button}>
+                    Buy
+                </button>
+            </div>
+            <div className={Style.image}>
+                <Image src={image} loader={myLoader} alt={{name} + 'image'} width={100} height={100} />
+            </div>
+            <div className={Style.nft}>
+                <div className={Style.nftInfo}>
+                <p className={Style.nftName}>
+                        {nftName}
+                    </p>
+                <div className={Style.author}>
+                    <Image src={avatar} loader={myLoader} alt={{name} + 'image'} width={100} height={100} />
+                    <p className={Style.authorName}>
+                        {name}
+                    </p>
+                </div>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
---
+++ src/components/testData.ts
@@ -0,0 +1,17 @@
+export type Product = {
+    name: string,
+    avatar: string,
+    price: string,
+    nftName: string,
+    id: string,
+    image: string,
+}
+
+export let testProduct = {
+    name: "Bryant Kuhn",
+    avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1169.jpg",
+    price: "9",
+    nftName: "dolore",
+    id: "1",
+    image: "https://loremflickr.com/640/480/abstract"
+}
