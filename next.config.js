32ac740486fd16b8b0c5e8a1bbeba5baaf42783e
Ilia Ustimov <ilia_ustimov@epam.com>
2022-12-15 22:35:52 +0400

    Теперь можно загружать с хоста картинки!

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'loremflickr.com',
+            port: '',
+            pathname: '640/480',
+        }, ],
+    },
 }
 
 module.exports = nextConfig
