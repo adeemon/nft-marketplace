7c830a59f392d97bcc18bf7d3540592b9883f98c
Ilia Ustimov <ilia_ustimov@epam.com>
2022-12-16 15:29:36 +0400

    Добавлена кнопка удаления товара, а также возможность перейти на страницу товара по клику на название

--- src/components/CartContent.tsx
+++ src/components/CartContent.tsx
@@ -1,17 +1,32 @@
+import { Button } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+import Link from 'next/link';
 import { useEffect } from 'react';
-import { selectCartProducts } from '../slices/CartSlice'
-import { useAppSelector } from '../store/store'
+import { removeProduct, selectCartProducts } from '../slices/CartSlice'
+import { useAppDispatch, useAppSelector } from '../store/store'
 import styles from '../styles/CartContent.module.scss'
 import { EthIcon } from './Eth';
 
 
 export default function CartContent () {
     const products = useAppSelector(selectCartProducts);
+    const dispatch = useAppDispatch();
+
+    const onDeleteProductHandler = (id : number) => {
+        dispatch(removeProduct(id));
+    }
+
     let totalPrice : number = 0;
     const productsArrayToRender = products.map(element => {
         totalPrice = totalPrice + Number(element.price);
         return (<div className={styles.product} key={element.id}>
-            <div className={styles.productName}>{element.nftName}</div>
+            <div className={styles.productName}>
+                <Button type="primary" shape="circle" icon={<DeleteOutlined />}
+                onClick={() => onDeleteProductHandler(element.id)} />
+                <Link href={`/products/${element.id}`}>
+                    {element.nftName}
+                </Link>
+                </div>
             <div className={styles.price}>
                 {element.price} <EthIcon />
                 </div>
